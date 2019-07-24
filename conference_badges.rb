12a213fc93542e30859750be2e8bcbbd10303685
jose luis <cortezluisjose@gmail.com>
2019-07-24 02:20:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,8 @@ def badge_maker(name)
 end
 def assign_rooms
   assign_rooms=["room 1", "room 2", "room 3", "room 4", "room 5", "room 6", "room 7"]
+  counter = 1 
   assign_rooms.each do |room|
+    puts ""
     puts  "Hello, _____! You'll be assigned to room _____!#{room}"
   end
