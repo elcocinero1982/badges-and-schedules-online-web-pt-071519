77814d463d791b502f8e7be2cc1b8fe7e77680a9
jose luis <cortezluisjose@gmail.com>
2019-07-24 01:55:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,6 @@
 # Write your code here.
-def badge_maker
+def badge_maker(name)
+  
   badge_maker.each do |"Arel|
-  puts "Hello, my name is Arel.#{badge_maker}"
+  puts "Hello, my name is Arel.#{Arel}"
 end
